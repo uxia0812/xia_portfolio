a3962a83590b2be408c4fd2efba7c0080c1b38b8
장유정 <306406j@gmail.com>
2024-05-01 21:06:37 +0900

    First Commit

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import HeroSection from "./components/HeroSection";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-[#121212]"></main>
+  );
+}
