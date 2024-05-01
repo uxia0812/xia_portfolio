66ee1b75abce2ae1087fc189c66ee2dfd1dca84f
장유정 <306406j@gmail.com>
2024-05-02 03:46:54 +0900

    HeroSection Style Edit

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -4,20 +4,29 @@ import Image from "next/image";
 const HeroSection = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7 place-self-center">
-          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-            {"Hello, I'm Ujeong"}
-            {/* 작은따옴표로 에러 발생해서 {""} 사이에 문자열 입력 */}
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              {"Hello, I'm"}{""}
+              {/* 작은따옴표로 에러 발생해서 {""} 사이에 문자열 입력 */}
+            </span>
+            Ujeong Jang
           </h1>
-          <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             As a passionate Full-Stack Developer, I blend deep technical
             expertise with a keen eye for design to create impactful web
             solutions.
           </p>
           <div>
-            <button className="px-6 py-2.5 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">Hire Me</button>
-            <button className="px-6 py-2.5 rounded-full mr-4 bg-transparent hover:bg-slate-800 text-white border border-white mt-2">Download CV</button>
+            <button className="px-6 py-2.5 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+              Hire Me
+            </button>
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-2">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </button>
           </div>
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
