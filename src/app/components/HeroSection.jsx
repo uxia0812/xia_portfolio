58396aee6437933a388a037e89a2b103d2f4735a
장유정 <306406j@gmail.com>
2024-05-02 03:06:01 +0900

    Add Image

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 const HeroSection = () => {
   return (
@@ -9,13 +10,24 @@ const HeroSection = () => {
             {"Hello, I'm Ujeong"}
             {/* 작은따옴표로 에러 발생해서 {""} 사이에 문자열 입력 */}
           </h1>
-          <p className="text-[#ADB7BE] text-lg lg:text-xl">
+          <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">
             As a passionate Full-Stack Developer, I blend deep technical
             expertise with a keen eye for design to create impactful web
             solutions.
           </p>
         </div>
-        <div className="col-span-5"></div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[400px] h-[300px] lg:w-[400px] lg:h-[300px] relative">
+            <Image
+              src="/images/mac_xia.png"
+              alt="Mac Xia"
+              // className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={400}
+              height={250}
+              className="blur-edge"
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
