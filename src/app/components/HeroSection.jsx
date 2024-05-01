37ec425513ccb1c74d71bb667b5a7a215aadb812
장유정 <306406j@gmail.com>
2024-05-01 21:33:58 +0900

    HeroSection <h1>, <p> 작성

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const HeroSection = () => {
   return (
     <section>
-      <h1 className="text-white">Hello, I'm Ujeong</h1>
+      <h1 className="text-white mb-4 text-6xl font-extrabold">
+        Hello, I'm Ujeong Jang
+      </h1>
+      <p className="text-[#ADB7BE]">
+        As a passionate Full-Stack Developer, I blend deep technical expertise with a keen eye for design to create impactful web solutions.
+      </p>
     </section>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
+    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4">
       <HeroSection />
     </main>
   );
