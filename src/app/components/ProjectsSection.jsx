6b2cbd61d7a470dc7183c7505d1d5cbd876f54d6
장유정 <306406j@gmail.com>
2024-05-04 02:22:27 +0900

    Style Edit

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -8,6 +8,8 @@ const ProjectData = [
     description: "Project 1 description",
     image: "/images/projects/portfolio_page.png",
     tag: ["Portfolio", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
   {
     id: 2,
@@ -15,6 +17,8 @@ const ProjectData = [
     description: "Project 2 description",
     image: "/images/projects/survey_app.png",
     tag: ["Survey", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
   {
     id: 3,
@@ -22,6 +26,8 @@ const ProjectData = [
     description: "Project 3 description",
     image: "/images/projects/note_ai_chat.png",
     tag: ["Note", "AI Chat"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
   {
     id: 4,
@@ -29,6 +35,8 @@ const ProjectData = [
     description: "Project 4 description",
     image: "/images/projects/multi_it_project.png",
     tag: ["Exhibition", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
   {
     id: 5,
@@ -36,6 +44,8 @@ const ProjectData = [
     description: "Project 5 description",
     image: "/images/projects/car_info.png",
     tag: ["Car", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
   {
     id: 6,
@@ -43,6 +53,8 @@ const ProjectData = [
     description: "Project 6 description",
     image: "/images/projects/my_ai_chat.png",
     tag: ["AI", "Chat"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
   {
     id: 7,
@@ -50,6 +62,8 @@ const ProjectData = [
     description: "Project 7 description",
     image: "/images/projects/todo_list.png",
     tag: ["Todo", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
   {
     id: 8,
@@ -57,20 +71,24 @@ const ProjectData = [
     description: "Project 8 description",
     image: "/images/projects/kahlua_page.png",
     tag: ["Menu", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
 ];
 
 const ProjectsSection = () => {
   return (
     <div>
-      <h2 className="text-center text-4xl font-bold text-white mt-4">My Projects</h2>
-      <div>
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8">My Projects</h2>
+      <div className="grid md:grid-cols-2 gap-6 md:gap-8">
         {ProjectData.map((project) => (
           <ProjectCard
             key={project.id}
             title={project.title}
             description={project.description}
             imgUrl={project.image}
+            gitUrl={project.gitUrl}
+            previewUrl={project.previewUrl}
           />
         ))}
       </div>
