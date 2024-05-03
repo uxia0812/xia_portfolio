ce8629a7be1bad44f28a148bf4d7d0089d9aced1
장유정 <306406j@gmail.com>
2024-05-04 04:10:15 +0900

    ProjectTag

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
 
 const ProjectData = [
   {
@@ -7,7 +10,7 @@ const ProjectData = [
     title: "React Portfolio Website",
     description: "Project 1 description",
     image: "/images/projects/portfolio_page.png",
-    tag: ["Portfolio", "Web"],
+    tag: ["All", "Front"],
     gitUrl: "/",
     previewUrl: "/",
   },
@@ -16,7 +19,7 @@ const ProjectData = [
     title: "Survey App",
     description: "Project 2 description",
     image: "/images/projects/survey_app.png",
-    tag: ["Survey", "Web"],
+    tag: ["All", "Back"],
     gitUrl: "/",
     previewUrl: "/",
   },
@@ -25,7 +28,7 @@ const ProjectData = [
     title: "Note + AI Chat Website",
     description: "Project 3 description",
     image: "/images/projects/note_ai_chat.png",
-    tag: ["Note", "AI Chat"],
+    tag: ["All", "Back"],
     gitUrl: "/",
     previewUrl: "/",
   },
@@ -34,7 +37,7 @@ const ProjectData = [
     title: "Exhibition Website",
     description: "Project 4 description",
     image: "/images/projects/multi_it_project.png",
-    tag: ["Exhibition", "Web"],
+    tag: ["All", "Back"],
     gitUrl: "/",
     previewUrl: "/",
   },
@@ -43,7 +46,7 @@ const ProjectData = [
     title: "Car Info Website",
     description: "Project 5 description",
     image: "/images/projects/car_info.png",
-    tag: ["Car", "Web"],
+    tag: ["All", "Back"],
     gitUrl: "/",
     previewUrl: "/",
   },
@@ -52,7 +55,7 @@ const ProjectData = [
     title: "My AI Chat Website",
     description: "Project 6 description",
     image: "/images/projects/my_ai_chat.png",
-    tag: ["AI", "Chat"],
+    tag: ["All", "Back"],
     gitUrl: "/",
     previewUrl: "/",
   },
@@ -61,7 +64,7 @@ const ProjectData = [
     title: "Todo List Website",
     description: "Project 7 description",
     image: "/images/projects/todo_list.png",
-    tag: ["Todo", "Web"],
+    tag: ["All", "Front"],
     gitUrl: "/",
     previewUrl: "/",
   },
@@ -70,24 +73,47 @@ const ProjectData = [
     title: "Kahlua Menu Website",
     description: "Project 8 description",
     image: "/images/projects/kahlua_page.png",
-    tag: ["Menu", "Web"],
+    tag: ["All", "Front"],
     gitUrl: "/",
     previewUrl: "/",
   },
 ];
 
 const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = ProjectData.filter((project) => 
+    project.tag.includes(tag)
+  );
+
   return (
     <div>
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8">
         My Projects
       </h2>
       <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
-        <button className="rounded-full border-2 border-purple-500 px-5 py-2 text-xl cursor-pointer">All</button>
-        <button className="rounded-full border-2 border-slate-600 hover:border-white px-5 py-2 text-xl cursor-pointer">Web</button>
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Front"
+          isSelected={tag === "Front"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Back"
+          isSelected={tag === "Back"}
+        />
       </div>
       <div className="grid md:grid-cols-2 gap-6 md:gap-8">
-        {ProjectData.map((project) => (
+        {filteredProjects.map((project) => (
           <ProjectCard
             key={project.id}
             title={project.title}
