a2879ee9cefd7d205bb039580a2f60a11e38294a
장유정 <306406j@gmail.com>
2024-05-04 17:19:52 +0900

    Create Style Purple Circle

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,9 +1,13 @@
 // import { EmailTemplate } from '../../../components/EmailTemplate';
+import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
+export async function POST(req, res) {
+  const { email, subject, message} = await req.body;
+  console.log(email, subject, message);
+
   try {
     const data = await resend.emails.send({
       from: "Ujeong <uxia99@kakao.com>",
--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -6,8 +6,9 @@ import Image from "next/image";
 
 const EmailSection = () => {
   return (
-    <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4">
-      <div>
+    <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
+      <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
+      <div className="">
         <h4 className="text-xl font-bold text-white my-2">{`Let's Connect`}</h4>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           {" "}
@@ -25,33 +26,57 @@ const EmailSection = () => {
         </div>
       </div>
       <div>
-        <form className="flex flex-col gap-4">
-          <label
-            htmlFor="email"
-            className="text-white block mb-2 text-sm font-medium"
+        <form className="flex flex-col">
+          <div className="mb-6">
+            <label
+              htmlFor="email"
+              className="text-white block mb-2 text-sm font-medium"
+            >
+              Your Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              required
+              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              placeholder="Please enter your email"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="subject"
+              className="text-white block text-sm mb-2 font-medium"
+            >
+              Subject
+            </label>
+            <input
+              type="text"
+              id="subject"
+              required
+              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              placeholder="Please send any questions"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="message"
+              className="text-white block text-sm mb-2 font-medium"
+            >
+              Message
+            </label>
+            <textarea
+              name="message"
+              id="message"
+              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              placeholder="Let's talk about.."
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
           >
-            Your Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            required
-            className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-            placeholder="Please enter your email"
-          />
-          <label
-            htmlFor="subject"
-            className="text-white block mb-2 text-sm font-medium"
-          >
-            Subject
-          </label>
-          <input
-            type="text"
-            id="subject"
-            required
-            className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-            placeholder="Please send any questions"
-          />
+            Send Message
+          </button>
         </form>
       </div>
     </section>
