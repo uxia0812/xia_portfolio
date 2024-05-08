4f17790dabd5ee399a25c19754711a1bb787223c
장유정 <306406j@gmail.com>
2024-05-09 02:34:27 +0900

    Edit Email Message

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -19,7 +19,7 @@ export async function POST(req, res) {
         <>
           <h1>{subject}</h1>
           <p>Thank you for contacting me!</p>
-          <p>New message submitted:</p>
+          <p>I will reply to you as soon as possible<br/></p>
           <p>{message}</p>
         </>
       ),
